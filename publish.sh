c51858688cb56d8ae6c2ab14c87ea500a6079fee
voronetskyy <voronetskyy@b2ab43da-b8a5-11dd-b55b-2da8d274f894>
2010-12-27 20:12:37 +0000

    add script to auto publishing by FTP (only for Unix based systems)
    git-svn-id: https://zvezda.googlecode.com/svn/trunk/ProffiCV@66 b2ab43da-b8a5-11dd-b55b-2da8d274f894

---
+++ publish.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ftp ftp://yevvor2:9655161@yev-java.freehostia.com << ftpEOF
+   prompt
+   put web/index.html yev-java.freehostia.com/index.html
+   put web/index_fr.html yev-java.freehostia.com/index_fr.html
+   put web/index_ru.html yev-java.freehostia.com/index_ru.html
+   put web/index_ua.html yev-java.freehostia.com/index_ua.html
+   lcd web/pdf
+   cd yev-java.freehostia.com/pdf/
+   mput *.pdf   
+   quit
+ftpEOF
