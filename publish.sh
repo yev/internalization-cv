6065fb7b134720609747804853bfdc8f12b3fc17
yev <voronetskyy_04@yahoo.fr>
2011-06-06 18:08:16 +0200

    add italian files to publish.sh script for automatic deployement

--- publish.sh
+++ publish.sh
@@ -8,6 +8,7 @@ ftp ftp://yevvor2:yuuc@ftp.freehostia.com << ftpEOF
    put web/index_fr.html yev-java.freehostia.com/index_fr.html
    put web/index_ru.html yev-java.freehostia.com/index_ru.html
    put web/index_ua.html yev-java.freehostia.com/index_ua.html
+   put web/index_it.html yev-java.freehostia.com/index_it.html
    lcd web/pdf
    cd yev-java.freehostia.com/pdf/
    mput *.pdf   
